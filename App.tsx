c943e34ab969260d9e95c108f46263962e72196c
Vaishnao Vaidya <vaishnaovaidya2001@gmail.com>
2023-12-15 18:27:02 +0530

    ScrollView

--- App.tsx
+++ App.tsx
@@ -1,23 +1,15 @@
-import React from 'react';
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import React from 'react'
+import FlatCards from './components/FlatCards'
 
-import {
- View, 
- Text,
- SafeAreaView
-} from 'react-native'
-
-function App(){
- return(
-  <SafeAreaView>
-  <View>
-    <Text>Hello World !</Text>
-    <Text>Hello World !</Text>
-    <Text>Hello World !</Text>
-    <Text>Hello World !</Text>
-    <Text>Hello World !</Text>
-  </View>
-</SafeAreaView>
- )
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <FlatCards />
+      </ScrollView>
+    </SafeAreaView>
+  )
 }
 
-export default App;
+export default App
